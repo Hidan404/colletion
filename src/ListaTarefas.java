1ff60de88b87ba700619e35107c3078e15027805
Ronald <ronaldkurouzo@gmail.com>
2024-05-15 23:37:51 -0300

    first commit

---
+++ src/ListaTarefas.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+  
+    List<Tarefa> yodo = new ArrayList<>();
+
+    
+
+    public ListaTarefas() {
+        this.yodo = new ArrayList<>();
+    }
+
+
+
+    public void adicionarTarefa(String descricao){
+        yodo.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        yodo.remove(0);
+    }
+
+    public int obterNumeroTarefas(){
+        return yodo.size();
+    }
+
+    public void obterDescricoesTarefas(){
+        for (Tarefa tarefa : yodo) {
+            System.out.println("A tarefa e: " + tarefa);
+        }
+    }
+}
---
+++ src/Tarefa.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tarefa  {
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    
+}
